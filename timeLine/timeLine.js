276fd620382637383a829fddd85eabd2353352fd
chris Bamborough <bambarooga@gmail.com>
2016-10-25 20:52:37 +1100

    Initial Commit

---
+++ timeLine/timeLine.js
@@ -0,0 +1,19 @@
+
+function setup(){
+  // create a canvas to draw in
+  var canvas = createCanvas(640,480);
+  canvas.background(255,255,255);
+
+
+  // input data in JSON format
+  //taskData = loadJSON("data.json")
+  //console.log(taskData);
+
+  // access the canvas in the DOM
+
+}
+
+function draw(){
+  //console.log("I'm working here");
+   ellipse(50, 50, 80, 80);
+}
